bc5280aa9cb75cc24b1f701219e99217044ec729
Yannick Maes <yannick.maes@nipro-group.com>
2025-03-20 09:29:59 +0100

    Modernize repo a bit (#4)
    * use pnpm + convert to ESM
    
    * add Typescript
    
    * bump to v2

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { getInput } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+import { readFileSync } from "node:fs";
+
+async function run() {
+  const content = getInput("content", { required: true });
+  const contentIsFilePath = getInput("contentIsFilePath");
+  const regex = getInput("regex") || "---.*";
+  const regexFlags = getInput("regexFlags") || "";
+  const token = getInput("token", { required: true });
+
+  const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY?.split("/") || [];
+  const prNumber = context.payload.issue?.number;
+
+  const octokit = getOctokit(token);
+
+  if (!repoOwner || !repoName || !prNumber) return;
+
+  const { data } = await octokit.rest.pulls.get({
+    owner: repoOwner,
+    repo: repoName,
+    pull_number: prNumber,
+  });
+
+  let body = data.body;
+
+  let output = content;
+  if (contentIsFilePath && contentIsFilePath === "true") {
+    output = readFileSync(content).toString("utf-8");
+  }
+
+  const re = RegExp(regex, regexFlags);
+  if (body && body.match(re)) {
+    body = body.replace(re, output);
+  } else if (body) {
+    body += output;
+  } else {
+    body = output;
+  }
+
+  await octokit.rest.pulls.update({
+    owner: repoOwner,
+    repo: repoName,
+    body: body,
+    pull_number: prNumber,
+  });
+}
+
+run();
